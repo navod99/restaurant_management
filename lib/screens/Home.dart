a6a85ae6494113a62ccabec710faf637c8de0122
navod99 <navx99@gmail.com>
2023-03-20 19:09:56 +0530

    add bottom navigation tabs

---
+++ lib/screens/Home.dart
@@ -0,0 +1,86 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:restaurant_management/screens/Login.dart';
+
+class Home extends StatefulWidget {
+  final User? user;
+  const Home(this.user, {super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _tabIndex = 0;
+  final List<String> _tabTitles = [
+    'Home',
+    'Cart',
+    'Account',
+  ];
+
+  static const List<Widget> _options = <Widget>[
+    Text('Home'),
+    Text('Cart'),
+    Text('Account'),
+  ];
+
+  void _onTapped(int index) {
+    setState(() {
+      _tabIndex = index;
+    });
+  }
+
+  Future<void> _logout() async {
+    await FirebaseAuth.instance.signOut();
+    if (mounted) {
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_tabTitles[_tabIndex]),
+      ),
+      // body: Center(
+      //     child: Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+      //     Text('Welcome, ${widget.user!.email}'),
+      //     const SizedBox(
+      //       height: 16,
+      //     ),
+      //     ElevatedButton(onPressed: _logout, child: const Text("Logout"))
+      //   ],
+      // )),
+      body: IndexedStack(
+        index: _tabIndex,
+        children: _options,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xFF6F35A5),
+        selectedItemColor: Colors.white,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+        currentIndex: _tabIndex,
+        onTap: _onTapped,
+      ),
+    );
+  }
+}
