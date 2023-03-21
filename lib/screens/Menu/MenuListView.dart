85144c86dcd1c1521a99630263f08e7d81877b17
Ashen Hettiarachchi <ashenirhettiarachchi@gmail.com>
2023-03-22 00:38:54 +0530

    removed unwanted file and added menu list view

---
+++ lib/screens/Menu/MenuListView.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MenuListView extends StatelessWidget {
+  const MenuListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
